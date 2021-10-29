5bed57161da04aa3a609f3711d5a238c5beb5f18
DaRealMcCloud <42445602+DaRealMcCloud@users.noreply.github.com>
2021-10-29 09:45:25 +0200

    update

--- Shell/router_run.sh
+++ Shell/router_run.sh
@@ -19,12 +19,12 @@ image=test.txt
  read -s -e password
  echo -ne '\n'
  echo -n "Enter your enable password "
- read -s -e enable
+ read -s -e enablepwd
  echo -ne '\n'
 
 # Pull in device list and passwords
 for device in `cat router_test_list.txt`; do
-    ./config.exp $user $device $password $enable ;
+    ./config.exp $user $device $password $enablepwd ;
     ./scp_trans.exp $user $device $password $image ;
     check=$(./verify.exp $user $device $password $image | grep check );
     echo "test0"
@@ -40,7 +40,7 @@ for device in `cat router_test_list.txt`; do
     if [ -n $checked ]
     then
         echo "Image OK, continue."
-        #./last_expect.exp $user $device $password $enable $image ;
+        #./last_expect.exp $user $device $password $enablepwd $image ;
     else
         echo "Image verification failed!"
         ## save hostname to file.
