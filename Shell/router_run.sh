4182a90c045887d376afeefb246c82b8340f55a2
DaRealMcCloud <42445602+DaRealMcCloud@users.noreply.github.com>
2021-11-02 09:13:45 +0100

    test

--- Shell/router_run.sh
+++ Shell/router_run.sh
@@ -37,7 +37,7 @@ for device in `cat router_test_list.txt`; do
 
     checked=$check | grep OK
     echo $checked
-    if [ -n $checked ]
+    if [ -z $checked ]
     then
         echo "Image OK, continue."
         #./last_expect.exp $user $device $password $enablepwd $image ;
